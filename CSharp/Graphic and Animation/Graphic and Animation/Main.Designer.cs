ae0bf7e740befda37322fc1d5f59ea15ca0df835
Nguyen Minh Nhat <nmnhat7399@gmail.com>
2022-10-17 23:20:32 +0700

    Hoan thanh bai 1

---
+++ CSharp/Graphic and Animation/Graphic and Animation/Main.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Graphic_and_Animation
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.lbNow = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.CtrlTimer = new System.Windows.Forms.Timer(this.components);
+            this.lbMotor = new System.Windows.Forms.Label();
+            this.btDirection = new System.Windows.Forms.Button();
+            this.tbSetLevel = new System.Windows.Forms.TextBox();
+            this.btConfirm = new System.Windows.Forms.Button();
+            this.lbLevel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.ForeColor = System.Drawing.Color.Red;
+            this.lbTitle.Location = new System.Drawing.Point(202, 30);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(393, 25);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "TANK MONITORING AND CONTROL";
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Enabled = true;
+            this.MainTimer.Interval = 1000;
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // lbNow
+            // 
+            this.lbNow.AutoSize = true;
+            this.lbNow.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNow.Location = new System.Drawing.Point(312, 67);
+            this.lbNow.Name = "lbNow";
+            this.lbNow.Size = new System.Drawing.Size(164, 18);
+            this.lbNow.TabIndex = 1;
+            this.lbNow.Text = "##/##/#### ##:##:## XX";
+            // 
+            // btStart
+            // 
+            this.btStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btStart.Location = new System.Drawing.Point(51, 141);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(67, 26);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "START";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btStart_MouseDown);
+            this.btStart.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btStart_MouseUp);
+            // 
+            // btStop
+            // 
+            this.btStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btStop.Location = new System.Drawing.Point(51, 189);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(67, 26);
+            this.btStop.TabIndex = 3;
+            this.btStop.Text = "STOP";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btStop_MouseDown);
+            this.btStop.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btStop_MouseUp);
+            // 
+            // CtrlTimer
+            // 
+            this.CtrlTimer.Enabled = true;
+            this.CtrlTimer.Tick += new System.EventHandler(this.CtrlTimer_Tick);
+            // 
+            // lbMotor
+            // 
+            this.lbMotor.AutoSize = true;
+            this.lbMotor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMotor.Location = new System.Drawing.Point(162, 170);
+            this.lbMotor.Name = "lbMotor";
+            this.lbMotor.Size = new System.Drawing.Size(39, 20);
+            this.lbMotor.TabIndex = 4;
+            this.lbMotor.Text = "###";
+            // 
+            // btDirection
+            // 
+            this.btDirection.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDirection.Location = new System.Drawing.Point(499, 141);
+            this.btDirection.Name = "btDirection";
+            this.btDirection.Size = new System.Drawing.Size(96, 26);
+            this.btDirection.TabIndex = 5;
+            this.btDirection.Text = "DIRECTION";
+            this.btDirection.UseVisualStyleBackColor = true;
+            this.btDirection.Click += new System.EventHandler(this.btDirection_Click);
+            // 
+            // tbSetLevel
+            // 
+            this.tbSetLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSetLevel.Location = new System.Drawing.Point(499, 189);
+            this.tbSetLevel.Name = "tbSetLevel";
+            this.tbSetLevel.Size = new System.Drawing.Size(100, 22);
+            this.tbSetLevel.TabIndex = 6;
+            this.tbSetLevel.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbSetLevel_KeyDown);
+            // 
+            // btConfirm
+            // 
+            this.btConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btConfirm.Location = new System.Drawing.Point(615, 188);
+            this.btConfirm.Name = "btConfirm";
+            this.btConfirm.Size = new System.Drawing.Size(76, 26);
+            this.btConfirm.TabIndex = 7;
+            this.btConfirm.Text = "COMFIRM";
+            this.btConfirm.UseVisualStyleBackColor = true;
+            this.btConfirm.Click += new System.EventHandler(this.btConfirm_Click);
+            // 
+            // lbLevel
+            // 
+            this.lbLevel.AutoSize = true;
+            this.lbLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLevel.Location = new System.Drawing.Point(499, 228);
+            this.lbLevel.Name = "lbLevel";
+            this.lbLevel.Size = new System.Drawing.Size(28, 16);
+            this.lbLevel.TabIndex = 8;
+            this.lbLevel.Text = "###";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbLevel);
+            this.Controls.Add(this.btConfirm);
+            this.Controls.Add(this.tbSetLevel);
+            this.Controls.Add(this.btDirection);
+            this.Controls.Add(this.lbMotor);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.lbNow);
+            this.Controls.Add(this.lbTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "SCADA with CSharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Label lbNow;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Timer CtrlTimer;
+        private System.Windows.Forms.Label lbMotor;
+        private System.Windows.Forms.Button btDirection;
+        private System.Windows.Forms.TextBox tbSetLevel;
+        private System.Windows.Forms.Button btConfirm;
+        private System.Windows.Forms.Label lbLevel;
+    }
+}
+
---
+++ CSharp/Graphic and Animation/Graphic and Animation/Main.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphic_and_Animation
+{
+    public partial class Main : Form
+    {
+        bool Start;
+        bool Stop;
+        bool Motor;
+        bool Direction;
+        int SetLevel;
+        int Level;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            DateTime Today = DateTime.Now;
+            Console.WriteLine(Today.ToString());
+            lbNow.Text = Today.ToString();
+            lbMotor.Text = Motor ? "Running" : "Stopped";
+            Console.WriteLine($"Direction State: {Direction}");
+            Console.WriteLine($"SetLevel is: {SetLevel}");
+            Console.WriteLine($"Level is: {Level}");
+            Console.WriteLine("===========================");
+
+            lbLevel.Text = Level.ToString();
+        }
+
+        private void btStart_MouseDown(object sender, MouseEventArgs e)
+        {
+            Start = true;
+        }
+
+        private void btStart_MouseUp(object sender, MouseEventArgs e)
+        {
+            Start = false;
+        }
+
+        private void btStop_MouseDown(object sender, MouseEventArgs e)
+        {
+            Stop = true;
+        }
+
+        private void btStop_MouseUp(object sender, MouseEventArgs e)
+        {
+            Stop= false;
+        }
+
+        private void CtrlTimer_Tick(object sender, EventArgs e)
+        {
+            Motor = (Start||Motor)&&!Stop;
+            if (Direction)
+            {
+                Level++;
+                if (Level > SetLevel)
+                    Level = SetLevel;
+            }
+            else
+            {
+                Level--;
+                if (Level < 0)
+                    Level = 0;
+            }
+        }
+
+        private void btDirection_Click(object sender, EventArgs e)
+        {
+            Direction = !Direction;
+        }
+
+        private void btConfirm_Click(object sender, EventArgs e)
+        {
+            int temp;
+            bool ret = Int32.TryParse(tbSetLevel.Text, out temp);//xu ly loi
+            if (ret)
+                SetLevel = temp;
+        }
+
+        private void tbSetLevel_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                int temp;
+                bool ret = Int32.TryParse(tbSetLevel.Text, out temp);//xu ly loi
+                if (ret)
+                    SetLevel = temp;
+            }
+        }
+    }
+}
